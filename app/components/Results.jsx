da41b6caa5e46958e4dba69cb7c0892d21a60467
Scott Henderson <scott.mc.henderson@gmail.com>
2023-06-21 18:17:44 -0700

    latest and greatest

--- app/components/Results.jsx
+++ app/components/Results.jsx
@@ -7,19 +7,39 @@ function Results({data, showInfo}) {
     if (!data) { // if results have been sent
         return (<div>Pending Search</div>)
     } else {
+        // Create table headers
         const headers = ['Start Letter', "End Letter", "Keyboard Path", "Distance"];
         let headersList = headers.map((item, index) => {
             return <th scope = "col" key = {index}>{item}</th>
         })
-        let pathInfo = data.slice(1, -1);
+
+        // Create data information
+        let pathInfo = data.slice(1, -1); // slice off first and last element of object
+        console.log(`pathInfo`);
         console.log(pathInfo);
+        
+        // let pathData = ''
+
+        // for (var i in pathData) {
+        //     pathData.push(
+        //         <tr>
+        //             <th scope = "row">{pathData[i]['start']}</th>
+        //             <td>{pathInfo[i]['end']}</td>
+        //             <td>{pathInfo[i]['path']}</td>
+        //             <td>{pathInfo[i]['distance']}</td>
+        //         </tr>
+        // )}
 
         console.log('result Table hit')
-        let name = data[0]['name']
+        let name = data[0]['name'];
+
+        let distanceTraveled = data[(data.length - 1)]['totalDistance'];
+
         return (
             <div>
                 <div className='container'>
                     <div>Search for name: {name} </div>
+                    <div>Distance traveled: {distanceTraveled} </div>
                     <table className = "table">
                         <thead>
                             <tr>
@@ -27,7 +47,14 @@ function Results({data, showInfo}) {
                             </tr>
                         </thead>
                         <tbody>
-                            {}
+                            { pathInfo.forEach(item => 
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>1st</td>
+                                <td>2nd</td>
+                                <td>3rd</td>
+                            </tr>
+                          )}
                         </tbody>
                     </table>
                 </div>
